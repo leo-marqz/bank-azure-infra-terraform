6f0cdfe98d81bbf91a5a7a044018677002915083
Elmer Marquez <62399619+leo-marqz@users.noreply.github.com>
2025-09-14 13:35:34 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,44 @@
+
+variable "azure_location" {
+  type = string
+  default = "westus2"
+  description = "West US 2"
+}
+
+variable "azure_location_slug" {
+  type = string
+  default = "west-us-2"
+}
+
+variable "app_service_plan_sku" {
+  type = string
+  default = "B1"
+}
+
+variable "mssql_server_sku" {
+  type = string
+  default = "Basic"
+}
+
+variable "bank_microservices_tags" {
+  type = map(string)
+  default = {
+    "Environment" = ""
+    "Owner"       = ""
+    "Email"       = ""
+    "Description"       = "Microservices with C# and Asp.Net in .Net 8"
+  }
+}
+
+# Credenciales de prueba temporales para base de datos
+
+variable "tmp_username" {
+  type = string
+  default = ""
+}
+
+variable "tmp_password" {
+  type = string
+  default = ""
+}
+
